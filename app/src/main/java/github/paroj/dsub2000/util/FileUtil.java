bceea22605e0d7e1d2f7b7b2cc4667361a856333
DrShajul <drshajul@gmail.com>
2025-10-12 10:23:52 +0530

    fix: Use unique filenames for embedded cover art

--- app/src/main/java/github/paroj/dsub2000/util/FileUtil.java
+++ app/src/main/java/github/paroj/dsub2000/util/FileUtil.java
@@ -214,6 +214,13 @@ public class FileUtil {
 			} else {
 				artFile = hexFile;
 			}
+
+			// If the cover art ID is the same as the song ID, it's embedded.
+			// In that case, use the song ID in the filename to make it unique.
+			if (entry.getCoverArt() != null && entry.getCoverArt().equals(entry.getId())) {
+				artFile = new File(albumDir, entry.getId() + ".jpg");
+			}
+
 			return artFile;
 		}
     }
