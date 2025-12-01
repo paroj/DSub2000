223ba687cc18a1ea0528e65ac7e61303e889414a
Faeb35 <fabian.naef@windowslive.com>
2025-12-01 13:29:39 +0100

    Implement playAll and intoduce StarredRepository part 2

--- app/src/main/java/github/paroj/dsub2000/domain/repository/StarredRepository.java
+++ app/src/main/java/github/paroj/dsub2000/domain/repository/StarredRepository.java
@@ -8,17 +8,29 @@ import github.paroj.dsub2000.domain.MusicDirectory;
 import github.paroj.dsub2000.service.MusicService;
 import github.paroj.dsub2000.util.ProgressListener;
 
-public final class StarredUtil {
-    public static MusicDirectory getStarredAlbumsList(MusicService service, Context context, ProgressListener progressListener) throws Exception {
-        return getStarredFilteredList(service, context, progressListener, true, false);
+public final class StarredRepository {
+
+    private final MusicService musicService;
+    private final Context context;
+
+    public StarredRepository(MusicService musicService, Context context) {
+        this.musicService = musicService;
+        this.context = context;
     }
 
-    public static MusicDirectory getStarredSongsList(MusicService service, Context context, ProgressListener progressListener) throws Exception {
-        return getStarredFilteredList(service, context, progressListener, false, true);
+    public MusicDirectory getStarredAlbums(ProgressListener progressListener) throws Exception {
+        return getFilteredStarredList(progressListener, true, false);
     }
 
-    private static MusicDirectory getStarredFilteredList(MusicService service, Context context, ProgressListener progressListener, boolean includeDirs, boolean includeFiles) throws Exception {
-        MusicDirectory result = service.getStarredList(context, progressListener);
+    public MusicDirectory getStarredSongs(ProgressListener progressListener) throws Exception {
+        return getFilteredStarredList(progressListener, false, true);
+    }
+
+    private MusicDirectory getFilteredStarredList(ProgressListener progressListener,
+                                                  boolean includeDirs,
+                                                  boolean includeFiles) throws Exception {
+
+        MusicDirectory result = musicService.getStarredList(context, progressListener);
         List<MusicDirectory.Entry> children = result.getChildren(includeDirs, includeFiles);
         return new MusicDirectory(children);
     }
