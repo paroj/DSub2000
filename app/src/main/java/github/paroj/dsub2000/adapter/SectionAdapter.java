b7736938b7597362bcd7557db0883cf53ef06719
Pavel Rojtberg <rojtberg@gmail.com>
2026-02-07 14:44:03 +0100

    fix edge-to-edge layout warnings

--- app/src/main/java/github/paroj/dsub2000/adapter/SectionAdapter.java
+++ app/src/main/java/github/paroj/dsub2000/adapter/SectionAdapter.java
@@ -435,7 +435,9 @@ public abstract class SectionAdapter<T> extends RecyclerView.Adapter<UpdateViewH
 						Window window = ((SubsonicFragmentActivity) context).getWindow();
 						window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
 						window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-						window.setStatusBarColor(colorPrimaryDark);
+						if (Build.VERSION.SDK_INT < 35) {
+							window.setStatusBarColor(colorPrimaryDark);
+						}
 					}
 					return true;
 				}
